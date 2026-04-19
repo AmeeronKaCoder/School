4cf06e4a6052b4dfac77f0ec132685eb0fc46582
Ajay Hello <hello.ajay.nirmal@gmail.com>
2026-04-19 22:05:37 +0530

    Update App.jsx
    changes done on app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Development branch - Ajay sharma / Ranjeet gupta</h1>
+          <h1>Development branch - Ajay sharma / Ranjeet gupta...changes done</h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
